01c5bfc70a5e829717114a6e95d1ce30399f0a4e
AnushaManda-27 <anushamanda27@gmail.com>
2021-06-06 22:00:16 +0530

    Added: Genrics all UC's

---
+++ StringMaxValueUC3.java
@@ -0,0 +1,28 @@
+public class StringMaxValueUC3 {
+
+	public static void main(String[] args) {
+
+		String[] s = {"Apple", "Peach", "Banana"}; 
+		toPrint(s);
+		System.out.println("Maximum String value is:");
+		System.out.println(max(s));
+
+	}
+	
+	public static <E extends Comparable<E>> E max(E... elements) {
+		E max = elements[0];
+		for (E element : elements) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+		return max;
+	}
+
+	private static <E> void toPrint(E[] a) {
+
+		for (E i:a) {
+				System.out.println(i);  
+		}
+	}
+}
